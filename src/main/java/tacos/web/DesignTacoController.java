8cde3cce80cdab9caa43122a515e116f9ddf4d8f
Daniel Modrzejewski <danio1118m@gmail.com>
2019-04-29 15:36:26 +0200

    add validation for orders

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import tacos.Ingredient;
 import tacos.Ingredient.Type;
 import lombok.extern.slf4j.Slf4j;
 
+import javax.validation.Valid;
 
 
 @Slf4j
@@ -44,9 +46,11 @@ public class DesignTacoController {
         return "design";
     }
     @PostMapping
-    public String processDesign(Taco design) {
-        // Save the taco design...
-        // We'll do this in chapter 3
+    public String processDesign(@Valid Taco design, Errors errors) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
+        // Save the taco design... ToDo
         log.info("Processing design: " + design);
         return "redirect:/orders/current";
     }
